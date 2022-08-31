01ab192b44569ba4a15f55d625cd011e948a2a2e
David Olarte <dagov1998@gmail.com>
2022-08-30 21:57:07 -0500

    Entrega Final

--- src/RegistroService.tsx
+++ src/RegistroService.tsx
@@ -7,9 +7,9 @@ const headers = {
 const url = "http://192.168.1.6:4000";
 
 export const getRegistros = async() => {
-    return await axios.get("http://192.168.1.8:4000/registro", { headers })
+    return await axios.get("http://10.90.51.83:4000/registro", { headers })
 }
 
 export const createRegistro = async(Registro: registro) => {
-    return await axios.post("http://192.168.1.8:4000/registro", Registro)
+    return await axios.post("http://10.90.51.83:4000/registro", Registro)
 }
